1730560caea31d6d5c6029568ee0c39b27b7c8b7
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2019-09-08 21:13:05 +1000

    Updates

---
+++ precache-manifest.ae75212928f405d5b7802f3d5b39ac18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0aa47d56922f5b63e09e1e97150bba9",
+    "url": "/swipe/index.html"
+  },
+  {
+    "revision": "fa7ca6bcdc1dc2e621b8",
+    "url": "/swipe/static/css/main.c6629f89.chunk.css"
+  },
+  {
+    "revision": "9ead7ef518535dde8498",
+    "url": "/swipe/static/js/2.04ca56f4.chunk.js"
+  },
+  {
+    "revision": "fa7ca6bcdc1dc2e621b8",
+    "url": "/swipe/static/js/main.ff60dc5c.chunk.js"
+  },
+  {
+    "revision": "2b441581e415a71e1bbb",
+    "url": "/swipe/static/js/runtime~main.ebc7d5a8.js"
+  }
+]);
