33a80ca5394d3b5354b0c62acc613da92f1e8f48
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2019-09-08 20:31:53 +1000

    updated App and index files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './components/navbar/navbar';
 import Header from './components/header/header';
 import Container from './components/container/container';
 import { hot } from 'react-hot-loader/root';
-import './app.css';
+import './App.css';
 
 const App = () => {
 	return (
